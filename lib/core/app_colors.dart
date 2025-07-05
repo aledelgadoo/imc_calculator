e532617cfdabae0d4965f1c4aa428ce5585c71c4
aledelgadoo <alejandrodelgadovalera@gmail.com>
2025-07-05 01:21:03 +0100

    Modificado el yaml con la info más basica de la APP. Añadida la paleta de colores que usaremos en un principio.

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF06947C);
+  static const Color secondary = Color(0xFF003D33);
+  static const Color accent = Color(0xFFBE4AAB);
+  
+  // Backgrounds 
+  static const Color background = Color(0xFF86CCC0);
+  static const Color backgroundComponent = Color(0xFF4BA193);
+  static const Color backgroundComponentSelected = Color.fromARGB(255, 0, 112, 103);
+}
